5de7449c42917a51f66670b140431a6484af5a31
27389 <273893361@qq.com>
2018-08-31 11:20:10 +0800

    生产者的assembly部署

--- dubbo-demo/dubbo-demo-provider/src/main/java/org/apache/dubbo/demo/provider/LocalZookeeperImpl.java
+++ dubbo-demo/dubbo-demo-provider/src/main/java/org/apache/dubbo/demo/provider/LocalZookeeperImpl.java
@@ -1,7 +1,8 @@
 package org.apache.dubbo.demo.provider;
 
+import org.apache.dubbo.config.annotation.Service;
 import org.apache.dubbo.demo.LocalZoopkeeperService;
-
+@Service
 public class LocalZookeeperImpl  implements LocalZoopkeeperService {
     @Override
     public String sayHello(String name) {
